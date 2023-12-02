b8d7daa214e15234cc1680240aca8070cf76898c
alisa <gggg_gggg_21@mail.ru>
2023-12-02 13:57:33 +0500

    документация, фиксированный  хедер

--- stm_site/Controllers/HomeController.cs
+++ stm_site/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace stm_site.Controllers
         {
             return View();
         }
+        public IActionResult Smik()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
